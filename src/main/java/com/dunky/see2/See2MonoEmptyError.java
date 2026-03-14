6851f9bf3e100ea2fba91ffe334461ea0cee38be
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2026-03-14 11:20:34 -0700

    subscriber - Mono

--- src/main/java/com/dunky/see2/See2MonoEmptyError.java
+++ src/main/java/com/dunky/see2/See2MonoEmptyError.java
@@ -1,12 +1,22 @@
 package com.dunky.see2;
 
 import com.dunky.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public class See2MonoEmptyError {
 
+    private static final Logger log = LoggerFactory.getLogger(See2MonoEmptyError.class);
+
     public static void main(String[] args) {
         getUsername(1)
+                .subscribe(Util.subscriber());
+
+        var list = List.of(10, 215, 3, 455, 555, 105, 206, 100, 1000);
+        Mono.fromSupplier(() -> sum(list))
                 .subscribe(Util.subscriber());
     }
 
@@ -17,5 +27,10 @@ public class See2MonoEmptyError {
           case 3 -> Mono.empty(); // No username for userId 3
           default -> Mono.error(new IllegalArgumentException("Invalid userId: " + userId));
       };
+    }
+
+    private static int sum(List<Integer> list) {
+        log.info("Finding the sum: {}", list);
+        return list.stream().mapToInt(Integer::intValue).sum();
     }
 }
